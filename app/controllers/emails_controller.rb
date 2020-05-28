e02f08cd067317a810f1c73422ed3a75d3e42fec
jessica leclercq <jessica_leclercq@hotmail.fr>
2020-05-28 17:43:02 +0200

    gmail like ajax

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -4,8 +4,44 @@ class EmailsController < ApplicationController
     @emails = Email.all
   end
 
+  def show
+    @email = Email.find(params[:id])
+    @email.update(read: true)
+    respond_to do |format|
+      format.html { redirect_to email_path(@email.id) }
+      format.js {}
+    end
+  end
+
+  def create
+    @email = Email.new(objet: Faker::Book.title, body: Faker::Lorem.paragraph)
+
+    if @email.save
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js {}
+      end
+    else
+      redirect_to root_path, flash: {error: "Email non créé"}
+    end
+  end
   
+  def update
+    @email = Email.find(params[:id])
+    @email.toggle(:read).save
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js {}
+    end
+  end
 
+  def destroy
+    @email = Email.find(params[:id])
+    @email.destroy
 
-
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js {}
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+
 Rails.application.routes.draw do
   root to: "emails#index"
   devise_for :users
